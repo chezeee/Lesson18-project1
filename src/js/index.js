a3336ff7d58e44c8de644a837f45379abd6effde
Ilya Yalymov <chezeee@gmail.com>
2023-02-16 00:42:16 +0300

    Added project collector - parcel

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+// import "../../index.html"
+
+// import "../css/fonts.css";
+// import "../css/normalize.css";
+// import "../css/style.css";
+// import "../css/media.css";
+
+// import woman from "../img/header/woman-with-dog.png";
+
+// const img = new Image();
+// img.src = woman;
